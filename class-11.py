6a3b3e4422d70b8b5a1c8b333f5d78bae6899658
robinsardar01568 <robinsardar01568@gmail.com>
2023-09-16 20:52:43 +0600

    update

---
+++ class-11.py
@@ -0,0 +1,22 @@
+## 16/09/23
+
+# github config 
+
+""" command prompt """
+# git config --global user.name "git user name"
+# git config --global user.email "git email address"
+#git config --list
+
+""" git bash """
+# git init
+# git add .
+# git commit -m "first commit"
+####### git branch -M main
+# git remote add origin https://github.com/robinsardar01568/.git
+# git push -u origin master
+
+""" add a new file from old repository folder """
+# git add .
+# git commit -m "first commit"
+# git push -u origin master
+
